88eb7333c1afddd9e38a86dff58d1b6b69d660a3
Vlad Surubariu <vladsurubariu@gmail.com>
2024-04-14 19:59:44 +0300

    Refactoring:
    - Add comments to improve code readability
    - Add user input
    - Check for INVALID data

--- project1/project1/source.cpp
+++ project1/project1/source.cpp
@@ -2,8 +2,30 @@
 
 int main()
 {
-  cAnimals animals;
+  /* Animals object */
+  clAnimals animals;
 
-  animals.fAnimalsAndSounds();
+  /* Variables init */
+  std::string animal_name = "";
+  int iAnimalID = INVALID;
+
+  /* Get input */
+  std::cout << "I wonder how this animal sounds like: ";
+  std::cin >> animal_name;
+  std::cout << std::endl;
+
+  /* Search the input in animal list. Return INVALId if it doesn't exist or index otherwise */
+  iAnimalID = animals.getAnimalIDByName(animal_name);
+
+  if (iAnimalID == INVALID)
+  {
+    std::cout << "Is that even an animal?"<<std::endl;
+  }
+  else
+  {
+    std::cout << "I know! The " << animal_name << " makes sound: " << animals.getAnimalSound(iAnimalID);
+  }
+
+  return 0;
 }
 
